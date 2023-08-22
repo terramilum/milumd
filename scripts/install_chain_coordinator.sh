0b4c16220848e3cc17c907765dba2c6d1a43319b
emrahm <emrahm>
2023-08-22 16:45:53 +0300

    develop intalling coordinator chain.

--- scripts/install_chain_coordinator.sh
+++ scripts/install_chain_coordinator.sh
@@ -1,40 +1,62 @@
 #!/bin/sh
 #set -o errexit -o nounset -o pipefail
 
+# DEFAULT_HOMEP=${HOMEP:-~/.mirumd}
+HOMEP=${HOMEP:-/mnt/volume_fra1_02/terramirum}
 PASSWORD=${PASSWORD:-12345678}
 STAKE=${STAKE_TOKEN:-TRM}
 FEE=${FEE_TOKEN:-uTRM}
-CHAIN_ID=${CHAIN_ID:-terramirum-localnet}
+CHAIN_ID=${CHAIN_ID:-terramirum-testnet}
 MONIKER=${MONIKER:-node001}
-FILENAME=${FILENAME:-"$HOME"/.mirumd/config/genesis.json}
-CONFIG=${CONFIG:-"$HOME"/.mirumd/config/config.toml}
+GENESIS=${GENESIS:-"$HOMEP"/config/genesis.json}
+APPTOML=${APPTOML:-"$HOMEP"/config/app.toml}
+CLIENTTOML=${CLIENTTOML:-"$HOMEP"/config/client.toml}
+CONFIG=${CONFIG:-"$HOMEP"/config/config.toml}
+IS_PROD=${IS_PROD:-true}
 
-rm -rf ~/.mirumd
 
-mirumd init --chain-id "$CHAIN_ID" "$MONIKER"
+rm -rf "$HOMEP"
+
+mirumd init --chain-id "$CHAIN_ID" "$MONIKER" --home "$HOMEP"
 # staking/governance token is hardcoded in config, change this
-sed -i "s/\"stake\"/\"$STAKE\"/" $FILENAME
+sed -i "s/\"stake\"/\"$STAKE\"/" $GENESIS
 # this is essential for sub-1s block times (or header times go crazy)
-if grep -F "time_iota_ms" $FILENAME
+if grep -F "time_iota_ms" $GENESIS
 then 
-    sed -i 's/"time_iota_ms": "1000"/"time_iota_ms": "100"/' $FILENAME
+    sed -i 's/"time_iota_ms": "1000"/"time_iota_ms": "100"/' $GENESIS
 else   
-   sed -i 's/"max_gas": "-1"/"time_iota_ms": "100"/' $FILENAME
+   sed -i 's/"max_gas": "-1"/"time_iota_ms": "100"/' $GENESIS
 fi
+
+# to enable the api server
+sed -i '/\[api\]/,+3 s/enable = false/enable = true/' $APPTOML
+# to change the voting_period
+jq '.app_state.gov.voting_params.voting_period = "600s"' $GENESIS > temp.json && mv temp.json $GENESIS
+
+# to change the inflation
+jq '.app_state.mint.minter.inflation = "0.100000000000000000"' $GENESIS > temp.json && mv temp.json $GENESIS
 
 # making 1 sec block time.
 sed -i 's/timeout_commit = "5s"/timeout_commit = "2s"/' $CONFIG
 
+for file in "$CONFIG" "$APPTOML" "$CLIENTTOML"; do
+    sed -i 's/localhost/0.0.0.0/' "$file"
+    sed -i 's/127.0.0.1/0.0.0.0/' "$file"
+done
 
-if ! mirumd keys show validator; then
-   (echo "$PASSWORD"; echo "$PASSWORD") | mirumd keys add validator 
+if [ "$IS_PROD" = true ]; then
+    sed -i 's/log_level = "info"/log_level = "main:info,state:info,*:error"/' $CONFIG 
+fi
+
+if ! mirumd keys show validator --home "$HOMEP"; then
+   (echo "$PASSWORD"; echo "$PASSWORD") | mirumd keys add validator --home "$HOMEP"
 fi
 # hardcode the validator account for this instance
-echo "$PASSWORD" | mirumd genesis add-genesis-account validator "10000000000000000$STAKE"
+echo "$PASSWORD" | mirumd genesis add-genesis-account validator "10000000000000000$STAKE" --home "$HOMEP"
 
 # submit a genesis validator tx
 ## Workraround for https://github.com/cosmos/cosmos-sdk/issues/8251
-(echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | mirumd genesis gentx validator "100000000000$STAKE" --chain-id="$CHAIN_ID" --amount="100000000000$STAKE"
+(echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | mirumd genesis gentx validator "100000000000$STAKE" --chain-id="$CHAIN_ID" --amount="100000000000$STAKE" --home "$HOMEP"
 ## should be:
 # (echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | mirumd gentx validator "100000000000$STAKE" --chain-id="$CHAIN_ID"
-mirumd genesis collect-gentxs
+mirumd genesis collect-gentxs --home "$HOMEP"
