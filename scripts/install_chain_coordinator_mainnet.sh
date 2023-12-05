b35a850c820506fc4e236addd5b7b2890f79aa9c
emrahm <emrahm>
2023-12-05 15:32:21 +0300

    install chain

---
+++ scripts/install_chain_coordinator_mainnet.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+#set -o errexit -o nounset -o pipefail
+
+# DEFAULT_HOMEP=${HOMEP:-~/.mirumd}
+# HOMEP=${HOMEP:-/mnt/volume_fra1_02/terramirum}
+HOMEP=${HOMEP:-~/.mirumd}
+PASSWORD=${PASSWORD:-12345678}
+STAKE=${STAKE_TOKEN:-TRM}
+FEE=${FEE_TOKEN:-uTRM}
+CHAIN_ID=${CHAIN_ID:-terramirum-1}
+MONIKER=${MONIKER:-main}
+GENESIS=${GENESIS:-"$HOMEP"/config/genesis.json}
+APPTOML=${APPTOML:-"$HOMEP"/config/app.toml}
+CLIENTTOML=${CLIENTTOML:-"$HOMEP"/config/client.toml}
+CONFIG=${CONFIG:-"$HOMEP"/config/config.toml}
+IS_PROD=${IS_PROD:-true}
+
+
+rm -rf "$HOMEP"
+
+mirumd init --chain-id "$CHAIN_ID" "$MONIKER" --home "$HOMEP"
+# staking/governance token is hardcoded in config, change this
+sed -i "s/\"stake\"/\"$STAKE\"/" $GENESIS
+# this is essential for sub-1s block times (or header times go crazy)
+if grep -F "time_iota_ms" $GENESIS
+then 
+    sed -i 's/"time_iota_ms": "1000"/"time_iota_ms": "500"/' $GENESIS
+fi
+
+# to enable the api server
+sed -i '/\[api\]/,+3 s/enable = false/enable = true/' $APPTOML
+# to change the voting_period
+jq '.app_state.gov.voting_params.voting_period = "600s"' $GENESIS > temp.json && mv temp.json $GENESIS
+
+# to change the inflation
+jq '.app_state.mint.minter.inflation = "0.030000000000000000"' $GENESIS > temp.json && mv temp.json $GENESIS
+jq '.app_state.mint.params.inflation_rate_change = "0.030000000000000000"' $GENESIS > temp.json && mv temp.json $GENESIS
+jq '.app_state.mint.params.inflation_max = "0.050000000000000000"' $GENESIS > temp.json && mv temp.json $GENESIS
+jq '.app_state.mint.params.inflation_min = "0.001000000000000000"' $GENESIS > temp.json && mv temp.json $GENESIS
+
+# making 1 sec block time.
+sed -i 's/timeout_commit = "5s"/timeout_commit = "3s"/' $CONFIG
+
+for file in "$CONFIG" "$APPTOML" "$CLIENTTOML"; do
+    sed -i 's/localhost/0.0.0.0/' "$file"
+    sed -i 's/127.0.0.1/0.0.0.0/' "$file"
+done
+
+if [ "$IS_PROD" = true ]; then
+    sed -i 's/log_level = "info"/log_level = "main:info,state:info,*:error"/' $CONFIG 
+fi
+
+if ! mirumd keys show validator --home "$HOMEP"; then
+   (echo "$PASSWORD"; echo "$PASSWORD") | mirumd keys add validator --home "$HOMEP"
+fi
+# hardcode the validator account for this instance
+echo "$PASSWORD" | mirumd genesis add-genesis-account validator "100000000000000000$STAKE" --home "$HOMEP"
+
+# submit a genesis validator tx
+## Workraround for https://github.com/cosmos/cosmos-sdk/issues/8251
+(echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | mirumd genesis gentx validator "1000000000000$STAKE" --chain-id="$CHAIN_ID" --amount="1000000000000$STAKE" --home "$HOMEP"
+## should be:
+# (echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | mirumd gentx validator "100000000000$STAKE" --chain-id="$CHAIN_ID"
+mirumd genesis collect-gentxs --home "$HOMEP"
---
+++ scripts/install_full_node_mainnet.sh
@@ -0,0 +1,60 @@
+# this is secret. Please chage it when intalling full node.
+# should be changed
+PASSWORD=${PASSWORD:-12345678} 
+# chain id to replace genesis file with existing one.
+# must be correct chain id equal to gitup folder name under networks repository.
+CHAIN_ID=${CHAIN_ID:-terramirum-1}
+# give any key name for full node name. This will be visible at block explorer as a validator name. 
+# should be changed. If no changes, you can change it at config.toml file with moniker name.
+MONIKER=${MONIKER:-nodex}
+# configuration file names. no need to change
+FILENAME=${FILENAME:-"$HOME"/.mirumd/config/genesis.json}
+CONFIG=${CONFIG:-"$HOME"/.mirumd/config/config.toml}  
+
+rm -rf ~/.mirumd
+
+mirumd init --chain-id "$CHAIN_ID" "$MONIKER"
+
+rm -rf "$HOME"/networks
+
+git clone https://github.com/terramirum/networks.git "$HOME"/networks
+
+SOURCE_GENESIS="$HOME"/networks/"$CHAIN_ID"/genesis.json
+
+result=$(stat $SOURCE_GENESIS)
+if [ $? -ne 0 ]; then
+  echo "Error: genesis file not found"
+  exit 1
+fi 
+
+cp -rf $SOURCE_GENESIS $FILENAME 
+
+PERSISTENT_PEERS_PATH="$HOME"/networks/"$CHAIN_ID"/persistent_peers
+
+result=$(stat $PERSISTENT_PEERS_PATH)
+if [ $? -ne 0 ]; then
+  echo "Error: genesis file not found"
+  exit 1
+fi   
+
+PERSISTENT_PEERS=$(cat $PERSISTENT_PEERS_PATH)
+
+# making 1 sec block time.
+sed -i 's/timeout_commit = "5s"/timeout_commit = "3s"/' $CONFIG
+sed -i "s/persistent_peers = \"\"/persistent_peers = \"$PERSISTENT_PEERS\"/" $CONFIG
+
+echo "NOTICE !!!!!"
+
+echo "Chain is starting. Node will be synchronized with name " $MONIKER
+echo "Follow belowing step."
+echo "1. Execute create_wallet.sh to create wallet. Please backup your mnemonic words"
+echo "2. Execute create_validator.sh to become validator node."
+
+echo "After chain start and being syched, then stop the program."
+echo 'Use "nohup mirumd start &" to run process at background. '
+echo "Enjoy !!!!!!!!!"
+
+sleep 5
+
+mirumd start
+
