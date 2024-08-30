0f200b166c90decb18dbc0ac6e6d70f36cca355f
emrahm <emrahm>
2024-08-31 00:32:41 +0300

    upgrade genesis parameters

--- scripts/install_chain_coordinator.sh
+++ scripts/install_chain_coordinator.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #set -o errexit -o nounset -o pipefail
 
-# DEFAULT_HOMEP=${HOMEP:-~/.mirumd}
+DEFAULT_HOMEP=${HOMEP:-~/.mirumd}
 HOMEP=${HOMEP:-/mnt/volume_fra1_02/terramirum}
+# HOMEP=${HOMEP:-~/.mirumd}
 PASSWORD=${PASSWORD:-12345678}
 STAKE=${STAKE_TOKEN:-MIRUM}
 FEE=${FEE_TOKEN:-uMIRUM}
@@ -14,7 +15,7 @@ CLIENTTOML=${CLIENTTOML:-"$HOMEP"/config/client.toml}
 CONFIG=${CONFIG:-"$HOMEP"/config/config.toml}
 IS_PROD=${IS_PROD:-true}
 
-
+rm -rf "$DEFAULT_HOMEP"
 rm -rf "$HOMEP"
 
 mirumd init --chain-id "$CHAIN_ID" "$MONIKER" --home "$HOMEP"
@@ -30,7 +31,10 @@ fi
 
 apt update
 apt install -y jq
-
+# Store the original permissions
+ORIGINAL_PERMISSIONS=$(stat -c "%a" $GENESIS)
+# read and write permissiion to the owner.
+chmod 644 $GENESIS
 # to enable the api server
 sed -i '/\[api\]/,+3 s/enable = false/enable = true/' $APPTOML
 # to change the voting_period
@@ -39,13 +43,22 @@ jq '.app_state.gov.voting_params.voting_period = "600s"' $GENESIS > temp.json &&
 # to change the inflation
 jq '.app_state.mint.minter.inflation = "0.100000000000000000"' $GENESIS > temp.json && mv temp.json $GENESIS
 
+jq '.app_state.provider.params.max_provider_consensus_validators = "260"' $GENESIS > temp.json && mv temp.json $GENESIS
+jq '.app_state.staking.params.max_validators = "300"' $GENESIS > temp.json && mv temp.json $GENESIS
+jq '.app_state.slashing.params.downtime_jail_duration = "6000s"' $GENESIS > temp.json && mv temp.json $GENESIS
+
 # making 1 sec block time.
 sed -i 's/timeout_commit = "5s"/timeout_commit = "2s"/' $CONFIG
+
+sed -i 's/minimum-gas-prices = ""/minimum-gas-prices = "0.0000001mirum"/' $APPTOML 
+
 
 for file in "$CONFIG" "$APPTOML" "$CLIENTTOML"; do
     sed -i 's/localhost/0.0.0.0/' "$file"
     sed -i 's/127.0.0.1/0.0.0.0/' "$file"
 done
+
+ 
 
 if [ "$IS_PROD" = true ]; then
     sed -i 's/log_level = "info"/log_level = "main:info,state:info,*:error"/' $CONFIG 
@@ -63,3 +76,5 @@ echo "$PASSWORD" | mirumd genesis add-genesis-account validator "100000000000000
 ## should be:
 # (echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | mirumd gentx validator "100000000000$STAKE" --chain-id="$CHAIN_ID"
 mirumd genesis collect-gentxs --home "$HOMEP"
+# Restore the original permissions
+chmod $ORIGINAL_PERMISSIONS $GENESIS
--- scripts/install_chain_coordinator_mainnet.sh
+++ scripts/install_chain_coordinator_mainnet.sh
@@ -29,7 +29,10 @@ fi
 
 apt update
 apt install -y jq
-
+# Store the original permissions
+ORIGINAL_PERMISSIONS=$(stat -c "%a" $GENESIS)
+# read and write permissiion to the owner.
+chmod 644 $GENESIS
 # to enable the api server
 sed -i '/\[api\]/,+3 s/enable = false/enable = true/' $APPTOML
 # to change the voting_period
@@ -42,9 +45,14 @@ jq '.app_state.mint.params.inflation_max = "0.015000000000000000"' $GENESIS > te
 jq '.app_state.mint.params.inflation_min = "0.001000000000000000"' $GENESIS > temp.json && mv temp.json $GENESIS
 jq '.app_state.mint.params.goal_bonded = "0.510000000000000000"' $GENESIS > temp.json && mv temp.json $GENESIS
 jq '.app_state.mint.params.blocks_per_year = "10519200"' $GENESIS > temp.json && mv temp.json $GENESIS
+jq '.app_state.provider.params.max_provider_consensus_validators = "260"' $GENESIS > temp.json && mv temp.json $GENESIS
+jq '.app_state.staking.params.max_validators = "300"' $GENESIS > temp.json && mv temp.json $GENESIS
+jq '.app_state.slashing.params.downtime_jail_duration = "6000s"' $GENESIS > temp.json && mv temp.json $GENESIS
 
 # making 1 sec block time.
 sed -i 's/timeout_commit = "5s"/timeout_commit = "3s"/' $CONFIG
+
+sed -i 's/minimum-gas-prices = ""/minimum-gas-prices = "0.0000001mirum"/' $APPTOML
 
 for file in "$CONFIG" "$APPTOML" "$CLIENTTOML"; do
     sed -i 's/localhost/0.0.0.0/' "$file"
@@ -67,3 +75,5 @@ echo "$PASSWORD" | mirumd genesis add-genesis-account validator "100000000000000
 ## should be:
 # (echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | mirumd gentx validator "100000000000$STAKE" --chain-id="$CHAIN_ID"
 mirumd genesis collect-gentxs --home "$HOMEP"
+# Restore the original permissions
+chmod $ORIGINAL_PERMISSIONS $GENESIS
