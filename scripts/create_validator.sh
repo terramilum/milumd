b287b448842a24dfa64e71e369eac23de70ec254
emrahm <emrahm>
2024-09-17 15:52:34 +0300

    create validator

--- scripts/create_validator.sh
+++ scripts/create_validator.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
+set -e
 
-# Check if MONIKER is provided as the first argument
-if [ -z "$1" ]; then
-    echo "Error: No moniker provided. Please provide a moniker as the first argument."
-    exit 1
+PASSWORD=$1
+
+# Path to the TOML file
+CONFIG_FILE="$HOME/.mirumd/config/config.toml"
+# Check if config file exists
+if [ -f "$CONFIG_FILE" ]; then
+    # Extract the moniker value
+    moniker=$(grep '^moniker' "$CONFIG_FILE" | cut -d'=' -f2 | sed 's/[ "]//g')
+    if [ -n "$moniker" ]; then
+        echo "Moniker found: $moniker"
+        NAME="$moniker"
+    fi
 fi
 
-MONIKER=$1
-
-# Check if the moniker is set to the placeholder "moniker"
-if [ "$MONIKER" == "moniker" ]; then
-    echo 'Error: Moniker name cannot be "moniker". Please choose a unique moniker.'
-    exit 1
+if [ -z "$NAME" ]; then
+  echo "Error: You must provide a validator moniker name for your wallet."
+  exit 1
 fi
 
-# Execute the create-validator command
-mirumd tx staking create-validator \
+# Ensure a wallet password is provided
+if [ -z "$PASSWORD" ]; then
+  echo "Error: You must provide a wallet password as the first argument."
+  exit 1
+fi
+
+# Execute the create-validator command with password input and auto-signing
+(echo "$PASSWORD"; echo "y") | mirumd tx staking create-validator \
   --amount=25000000000000MIRUM \
   --pubkey=$(mirumd tendermint show-validator) \
-  --moniker="$MONIKER" \
+  --moniker="$NAME" \
   --chain-id=mirum-1 \
   --commission-rate="0.10" \
   --commission-max-rate="0.20" \
   --commission-max-change-rate="0.01" \
   --min-self-delegation="25000000000000" \
   --gas="2500000" \
-  --gas-prices="0.01MIRUM" \
-  --from=validator
+  --gas-prices="0.1MIRUM" \
+  --from=$NAME \
+  --yes
